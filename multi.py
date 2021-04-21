dab4cd8461151f92aa3eabef7ad67a97b2ee293b
vaibhavnaughty <47244162+vaibhavnaughty@users.noreply.github.com>
2021-04-21 15:55:42 +0530

    Final
    Final commit

---
+++ multi.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+M1 = np.array([[3, 6], [5, -10]])
+M2 = np.array([[9, -18], [11, 22]])
+M3 = M1.dot(M2)  
+print(M3)
---
+++ transpose.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+M1 = np.array([[3, 6, 9], [5, -10, 15], [4,8,12]])
+M2 = M1.transpose()
+
+print(M2)
